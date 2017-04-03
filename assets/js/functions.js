6fa3c60574086da00cbc83a27f412922df6a31d8
Masami Okada <mokada42991@gmail.com>
2017-04-03 14:19:10 +0200

    Services section text, smooth scrolling

---
+++ assets/js/functions.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    //console.log("masami okada is here");
+
+    $(".learnMore").on("click", function() {
+        $("html, body").animate({
+            scrollTop: $("#about").offset().top
+        }, 1500);
+    });
+});
