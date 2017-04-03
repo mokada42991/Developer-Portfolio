2d18bb27e2fc2036800095e200c94667495f0c0b
Masami Okada <mokada42991@gmail.com>
2017-04-03 22:26:27 +0200

    Tools section, grid

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     $(".learnMore").on("click", function() {
         $("html, body").animate({
-            scrollTop: $("#about").offset().top
-        }, 1500);
+            scrollTop: $(".about").offset().top
+        }, 1700, "swing");
     });
 });
