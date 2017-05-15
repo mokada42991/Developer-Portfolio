707b067450c9e8f587b4a0ecd58305906836222a
Masami Okada <mokada42991@gmail.com>
2017-05-15 20:25:04 +0200

    jQuery Easing Plugin added

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
     $(".learnMore").on("click", function() {
         $("html, body").animate({
             scrollTop: $(".about").offset().top
-        }, 1700, "swing");
+        }, 1700, "easeInOutExpo");
     });
 
     $(".contact-button").on("click", function() {
         $("html, body").animate({
             scrollTop: $(".contact").offset().top
-        }, 1700, "swing");
+        }, 2000, "easeInOutExpo");
     });
 });
